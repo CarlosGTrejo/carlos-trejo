40909e3b83a27ff757622e22f6ba9724365ad7b7
Carlos Trejo <37058445+CarlosGTrejo@users.noreply.github.com>
2025-06-09 15:42:48 -0500

    Created error page, added tailwind to built dependencies

---
+++ src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { page } from '$app/state';
+</script>
+
+<div class="error">
+	<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">{page.status}: {page.error?.message}</h1>
+</div>
+
+<style>
+	.error {
+		/* height: 100%; */
+		display: grid;
+		place-content: center;
+	}
+</style>
