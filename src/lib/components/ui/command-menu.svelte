deb963ff220990e86286fe3724a6d61e361b8c77
Carlos Trejo <37058445+CarlosGTrejo@users.noreply.github.com>
2025-06-30 09:58:32 -0500

    update command menu to display platform-specific key for search

--- src/lib/components/ui/command-menu.svelte
+++ src/lib/components/ui/command-menu.svelte
@@ -5,6 +5,10 @@
 	import { Newspaper, NotepadText, Sparkle } from '@lucide/svelte/icons';
 	import { onMount } from 'svelte';
 	import { Skeleton } from '$lib/components/ui/skeleton/index.js';
+	import { browser } from '$app/environment';
+
+	const platform = browser && window.navigator.platform;
+	const key = platform === 'MacIntel' ? '⌘' : 'Ctrl';
 
 	let open = $state(false);
 	let workerStatus = $state<'idle' | 'init' | 'ready'>('idle');
@@ -70,9 +74,15 @@
 <p class="text-muted-foreground text-sm max-lg:hidden">
 	Press
 	<kbd
-		class="bg-muted text-muted-foreground pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border px-1.5 font-mono text-[10px] font-medium opacity-100"
+		class="bg-muted text-muted-foreground pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border px-1 font-mono text-xs font-medium"
 	>
-		<span class="text-xs">⌘</span>K
+		{key}
+	</kbd>
+
+	<kbd
+		class="bg-muted text-muted-foreground pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border px-1.5 font-mono text-xs font-medium"
+	>
+		K
 	</kbd>
 	to search
 </p>
