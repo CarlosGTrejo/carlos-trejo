c73529466ac7172002b6162a45fe6b03376c874a
Carlos Trejo <37058445+CarlosGTrejo@users.noreply.github.com>
2025-06-30 10:46:54 -0500

    update project skills and descriptions in portfolio

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,22 +18,23 @@
 			skills: [
 				'SvelteKit',
 				'Svelte',
+				'Vercel',
+				'Docker',
 				'TypeScript',
 				'Tailwind CSS',
 				'Authentication',
 				'Custom API',
 				'Drizzle ORM',
-				'Vercel',
-				'PostgreSQL',
-				'Docker'
+				'PostgreSQL'
 			]
 		},
 		{
 			title: 'Personal Website',
 			description:
-				'Implemented advanced search functionality running on a web worker, dynamic content routing, and performance-optimized architecture. Focused on responsive design for enhanced user experience and deployed on CloudFlare Workers.',
+				'Implemented advanced search functionality running on a web worker, dynamic content routing, and a custom API. Focused on responsive design for enhanced user experience and deployed on CloudFlare Workers.',
 			skills: [
 				'SvelteKit',
+				'Svelte',
 				'TypeScript',
 				'Tailwind CSS',
 				'CloudFlare Workers',
@@ -49,6 +50,7 @@
 				'Virtualized environment built on Proxmox hypervisor, hosting various services including a high-availability Kubernetes cluster, XDR security system, and a public-facing game server with robust networking and DNS management.',
 			skills: [
 				'Proxmox',
+				'LXC Containers',
 				'Security and Monitoring',
 				'Networking',
 				'K0s (Kubernetes)',
@@ -66,7 +68,8 @@
 				'SIEM',
 				'Linux (RedHat)',
 				'Windows',
-				'Security best practices'
+				'Security best practices',
+				'Group Policy'
 			]
 		},
 		{
@@ -76,15 +79,14 @@
 			skills: [
 				'Linux (Debian)',
 				'Incremental Backups',
-				'Custom Systemd Services',
+				'Systemd Services',
 				'Cron Jobs',
-				'Networking',
-				'Security',
+				'SSH Hardening',
 				'Monitoring (Uptime Kuma)',
 				'Zero-Trust Access Control (TwinGate)',
 				'Documentation',
 				'Cloudflare Tunnel',
-				'Resource Management and Optimization'
+				'Resource Management + Optimization'
 			]
 		},
 		{
@@ -102,10 +104,10 @@
 				'Pandas',
 				'Scrapy',
 				'Matplotlib',
-				'Jupyter Notebooks',
 				'Data Scraping',
 				'Data Wrangling',
-				'Data Exploration'
+				'Data Exploration',
+				'Jupyter Notebooks'
 			]
 		}
 	];
@@ -113,9 +115,10 @@
 
 <section class="prose">
 	<p class="mt-0">
-		Hi, I'm Carlos and I enjoy exploring new technologies and building custom solutions. I'm interested in a little bit of everything, but mainly cybersecurity,
-		software development, and data science. In my free time, I like to work on personal projects,
-		learn new tools/skills, and stay updated with industry trends.
+		Hi, I'm Carlos and I enjoy exploring new technologies and building custom solutions. I'm
+		interested in a little bit of everything, but mainly cybersecurity, software development, and
+		data science. In my free time, I like to work on personal projects, learn new tools/skills, and
+		stay updated with industry trends.
 	</p>
 	<p>
 		I'm currently focused on enhancing my skills in these areas. This website serves as a portfolio
